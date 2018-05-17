879618bb64276ce1dd80963dd1229e9d49c41f4a
Sri Santhosh Hari <shari@dons.usfca.edu>
2018-05-17 04:05:15 -0700

    update functions and example

--- simulated_annealing.py
+++ simulated_annealing.py
@@ -3,18 +3,40 @@ import numpy as np
 from collections import OrderedDict
 from random import random
 
+
+# Sample train_model function #
+###############################
+
+# def train_model(curr_params, param, Xtrain, Xvalid, Ytrain,
+#                 Yvalid, metric=f1_score):
+#     """
+#     Train the model with given set of hyperparameters
+#     curr_params - Dict of hyperparameters and chosen values
+#     param - Dict of hyperparameters that are kept constant
+#     Xtrain - Train Data
+#     Xvalid - Validation Data
+#     Ytrain - Train labels
+#     Yvalid - Validaion labels
+#     metric - Metric to compute model performance on
+#     """
+#     params_copy = param.copy()
+#     params_copy.update(curr_params)
+#     model = XGBClassifier(**params_copy)
+#     model.fit(Xtrain, Ytrain)
+#     preds = model.predict(Xvalid)
+#     metric_val = metric(Yvalid, preds)
+#     return model, metric_val
+
+
 def choose_params(tune_dic, curr_params=None):
     """
     Function to choose parameters for next iteration
-    
     Inputs:
     tune_dic - Dict of Hyperparameter search space
     curr_params - Dict of current hyperparameters
-    
     Output:
     Dictionary of parameters
     """
-    
     if curr_params:
         next_params = curr_params.copy()
         param_to_update = np.random.choice(list(tune_dic.keys()))
@@ -26,7 +48,7 @@ def choose_params(tune_dic, curr_params=None):
             next_params[param_to_update] = param_vals[curr_index - 1]
         else:
             next_params[param_to_update] = \
-                param_vals[curr_index + np.random.choice([-1,1])]
+                param_vals[curr_index + np.random.choice([-1, 1])]
     else:
         next_params = dict()
         for k, v in tune_dic.items():
@@ -35,36 +57,37 @@ def choose_params(tune_dic, curr_params=None):
     return next_params
 
 
-def simulate_annealing(fn_train,
-                       tune_dic,
+def simulate_annealing(tune_dic,
                        const_param,
                        X_train,
                        X_valid,
-                       Y_train=None,
-                       Y_valid=None,
+                       Y_train,
+                       Y_valid,
+                       fn_train,
                        maxiters=100,
                        alpha=0.85,
                        beta=1.3,
                        T=0.40,
-                       update_iters=5,
-                       fn_train_args=None):
+                       update_iters=5):
     """
     Function to perform hyperparameter search using simulated annealing
-    
     Inputs:
-    fn_train - Function to train the model (Should return model and metric value)
     tune_dic - Dictionary of Hyperparameter search space
+    const_param - Static parameters of the model
+    Xtrain - Train Data
+    Xvalid - Validation Data
+    Ytrain - Train labels
+    Yvalid - Validaion labels
+    fn_train - Function to train the model
+        (Should return model and metric value as tuple, sample commented above)
     maxiters - Number of iterations to perform the parameter search
     alpha - factor to reduce temperature
     beta - constant in probability estimate
     T - Initial temperature
     update_iters - # of iterations required to update temperature
-    fn_train_args - Dictionary of non-trivial parameters (Everything except curr_params, param, X_train, X_valid)
-    
     Output:
     Dataframe of the parameters explored and corresponding model performance
     """
-
     columns = [*tune_dic.keys()] + ['Metric', 'Best Metric']
     results = pd.DataFrame(index=range(maxiters), columns=columns)
     best_metric = -1.
@@ -86,43 +109,42 @@ def simulate_annealing(fn_train,
                 hash_values.add(hash_val)
                 break
 
-        if fn_train_args:
-            model, metric = fn_train(curr_params, const_param, X_train, X_valid, **fn_train_args)
-        else:
-            model, metric = fn_train(curr_params, const_param, X_train, X_valid)
+        model, metric = fn_train(curr_params, const_param, X_train,
+                                 X_valid, Y_train, Y_valid)
 
         if metric > prev_metric:
-            print('Local Improvement in metric from {:8.4f} to {:8.4f} - parameters accepted'\
-                  .format(prev_metric, metric))
+            print('Local Improvement in metric from {:8.4f} to {:8.4f} '
+                  .format(prev_metric, metric) + ' - parameters accepted')
             prev_params = curr_params.copy()
             prev_metric = metric
 
             if metric > best_metric:
-                print('Global improvement in metric from {:8.4f} to {:8.4f} - best parameters updated'\
-                  .format(best_metric, metric))
+                print('Global improvement in metric from {:8.4f} to {:8.4f} '
+                      .format(best_metric, metric) +
+                      ' - best parameters updated')
                 best_metric = metric
                 best_params = curr_params.copy()
+                best_model = model
         else:
             rnd = np.random.uniform()
             diff = metric - prev_metric
             threshold = np.exp(beta * diff / T)
             if rnd < threshold:
-                print(
-                    """No Improvement but parameters accepted. Metric change: {:8.4f} 
-                threshold: {:6.4f} random number: {:6.4f}
-                """.format(diff, threshold, rnd))
+                print('No Improvement but parameters accepted. Metric change' +
+                      ': {:8.4f} threshold: {:6.4f} random number: {:6.4f}'
+                      .format(diff, threshold, rnd))
                 prev_metric = metric
                 prev_params = curr_params
             else:
-                print(
-                    """No Improvement and parameters rejected. Metric change: {:8.4f} 
-                threshold: {:6.4f} random number: {:6.4f}
-                """.format(diff, threshold, rnd))
+                print('No Improvement and parameters rejected. Metric change' +
+                      ': {:8.4f} threshold: {:6.4f} random number: {:6.4f}'
+                      .format(diff, threshold, rnd))
 
         results.loc[i, list(curr_params.keys())] = list(curr_params.values())
         results.loc[i, 'Metric'] = metric
         results.loc[i, 'Best Metric'] = best_metric
 
-        if i % update_iters == 0: T = alpha * T
+        if i % update_iters == 0:
+            T = alpha * T
 
-    return results
+    return results, best_model
